f33b7b481ac4cc6e85b46f5107aa24a62c3b2b47
Ed Minnix <egregius313@github.com>
2022-09-20 14:48:22 -0400

    Move help messages into comments to support `cargo doc`
    `cargo doc` cannot pickup `#[clap(help=...)]` as the documentation for
    an argument.

--- src/opts.rs
+++ src/opts.rs
@@ -7,21 +7,13 @@ use clap::Parser;
 
 #[derive(Debug, clap::Parser)]
 pub struct Opts {
-    #[clap(help = "Read from a file instead of stdin")]
+    /// Read from a file instead of stdin
     pub filename: Option<String>,
-    #[clap(
-        help = "Number of first lines of a file to display",
-        short = 'H',
-        long = "head",
-        default_value_t = 10
-    )]
+    /// Number of first lines of a file to display
+    #[clap(short = 'H', long = "head", default_value_t = 10)]
     pub head: usize,
-    #[clap(
-        help = "Number of last lines of a file to display",
-        short = 'T',
-        long = "tail",
-        default_value_t = 10
-    )]
+    /// Number of last lines of a file to display
+    #[clap(short = 'T', long = "tail", default_value_t = 10)]
     pub tail: usize,
     #[clap(
         help = "Wait for additional data to be appended to a file. Ignored if standard input is a pipe.",
