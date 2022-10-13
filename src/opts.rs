8ad9ac37010e80ec1fb23515c94316620be5e8ee
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-10-14 02:17:35 +0700

    upgrade to clap v4

--- src/opts.rs
+++ src/opts.rs
@@ -10,24 +10,24 @@ pub struct Opts {
     /// Read from a file instead of stdin
     pub filename: Option<String>,
     /// Number of first lines of a file to display
-    #[clap(short = 'H', long, default_value_t = 10)]
+    #[arg(short = 'H', long, default_value_t = 10)]
     pub head: usize,
     /// Number of last lines of a file to display
-    #[clap(short = 'T', long, default_value_t = 10)]
+    #[arg(short = 'T', long, default_value_t = 10)]
     pub tail: usize,
     /// Wait for additional data to be appended to a file. Ignored if standard
     /// input is a pipe. If a `notify`-compatible filesystem watcher is
     /// available, that will be used. If not, we will fall back to a polling
     /// watcher.
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub follow: bool,
     /// When following a file, sleep this amount in seconds between polling for changes. Ignored if
     /// a `notify`-compatible watcher is available.
-    #[clap(short, long, default_value_t = 0.025)]
+    #[arg(short, long, default_value_t = 0.025)]
     pub sleep_interval: f64,
 
     /// Write output to file
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub outfile: Option<String>,
 }
 
