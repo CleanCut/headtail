91d3ab8c61191ddadcc170976f1ffdd93d8c0c32
Nathan Stocks <cleancut@github.com>
2025-05-20 17:36:53 -0600

    switch from deprecated criterion::black_box to std::hint::black_box

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,4 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion};
 use headtail::{headtail, opts::Opts};
 
 pub fn c1(c: &mut Criterion) {
